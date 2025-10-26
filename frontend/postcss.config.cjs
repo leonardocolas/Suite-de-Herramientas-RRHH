7ff08d1b4d9c1219ab7a87d5e7ee955dd5a67ee4
Leonardo Colás Leyva <106391914+leonardocolas@users.noreply.github.com>
2025-10-26 05:44:54 -0400

    Creacion

---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // Indica a PostCSS que ejecute el plugin de Tailwind
+    tailwindcss: {},
+    // Indica a PostCSS que ejecute el plugin de Autoprefixer
+    autoprefixer: {},
+  },
+}
